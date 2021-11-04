8aca161be6ce438f80eff37dc193ef31fb87054b
Alex macbookAir <aleksandrmisnov@gmail.com>
2021-11-04 14:39:49 +0000

    Working on card app, added email, phone avatar

--- lib/main.dart
+++ lib/main.dart
@@ -8,10 +8,100 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        home: Scaffold(
+      backgroundColor: Colors.teal,
+      body: SafeArea(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50,
+                backgroundColor: Colors.teal,
+                child: ClipOval(
+                  child: Image.asset(
+                    'assets/avatar.png',
+                    width: 100,
+                    height: 100,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              Text(
+                'Stewie Griffin',
+                style: TextStyle(
+                  fontFamily: 'Roboto',
+                  fontSize: 40,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'REACT DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  fontSize: 20,
+                  color: Colors.teal.shade100,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                padding: EdgeInsets.all(10),
+                width: double.infinity,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(
+                      Icons.phone,
+                      size: 15,
+                      color: Colors.teal.shade800,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text('+44 1231231',
+                        style: TextStyle(
+                          fontFamily: 'Source Sans Pro',
+                          fontSize: 20,
+                          color: Colors.teal.shade800,
+                          letterSpacing: 2.5,
+                          fontWeight: FontWeight.bold,
+                        ))
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                padding: EdgeInsets.all(10),
+                width: double.infinity,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(
+                      Icons.email,
+                      size: 15,
+                      color: Colors.teal.shade800,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text('aleksandrmisnov@gmail.com',
+                        style: TextStyle(
+                          fontFamily: 'Source Sans Pro',
+                          fontSize: 15,
+                          color: Colors.teal.shade800,
+                          letterSpacing: 2.5,
+                          fontWeight: FontWeight.bold,
+                        ))
+                  ],
+                ),
+              ),
+            ]),
       ),
-    );
+    ));
   }
 }
